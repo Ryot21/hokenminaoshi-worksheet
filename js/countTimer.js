19832d157f85d8dec6847c66f04280ea15da6809
Ryot21 <rh.bicmac@gmail.com>
2025-01-27 14:21:02 +0900

    キャンペーン期間変更

--- js/countTimer.js
+++ js/countTimer.js
@@ -2,11 +2,12 @@
 
 const update = () => {
     // 日本時間の timezone 付きで指定
-    const targetTime = new Date('2025-01-16T00:00+09:00').getTime();
+    const targetTime = new Date('2025-01-27T00:00+09:00').getTime();
     const diff = targetTime - Date.now();
 
     // 残り日数を計算
-    const remainingDays = Math.floor(diff / 1000 / 60 / 60 / 24);
+    // const remainingDays = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const remainingDays = 0;
 
     // 残日数をHTML内に表示
     const dayElement = document.querySelector('#timer > .num');
